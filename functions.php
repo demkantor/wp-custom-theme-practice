4dd188b1f3745388156f995d48943929f3c641ea
demkantor <demkantor@gmail.com>
2020-07-07 16:13:46 -0500

    posts and comments now display dynamically

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function demkantor_theme_support(){
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'demkantor_theme_support');
